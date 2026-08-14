4f282cb4d95431d3fecb5c00bcc016a32ed31d00
mttkst717 <mttkst717@gmail.com>
2026-08-14 22:05:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil((async()=>{const k=await caches.keys();await Promise.all(k.map(x=>caches.delete(x)));await self.registration.unregister()})()));self.addEventListener("fetch",()=>{});
+self.addEventListener("install",e=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil((async()=>{
+ const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));
+ await self.registration.unregister();
+ const cs=await self.clients.matchAll({type:"window"});cs.forEach(c=>c.navigate(c.url));
+})()));
+self.addEventListener("fetch",()=>{});
